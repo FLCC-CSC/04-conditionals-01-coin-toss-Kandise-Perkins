68c3ec645e45205ddc456794e876c33c0053705b
Kandise-Perkins <kperkins10@fingerlakes.edu>
2025-10-06 02:29:49 +0000

    comment

--- coin_toss.py
+++ coin_toss.py
@@ -1,23 +1,44 @@
 # FILE NAME - coin_toss.py
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+
+# NAME: Kandise Perkins
+# DATE: October 5, 2025
+# BRIEF DESCRIPTION: generate a coin toss, if user enters numbers
+#   51 or greater make it Tails. 
+
+
+
 # 1. Make sure you fill out the comments above
 # 2. Write your code in the proper spot
 # 3. Be sure to answer the Reflection Questions and Attestation below
 # 4. The Sample Output has been included in this code for your convenience
+
+import random
+
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
+# Don't forget to import random!!!!!
+def main():
+    coin_flip()
+def coin_flip():
+    print('===== Coin Flipper =====')
+
+    #random number 1-100
+    number = random.randint(1,100)
+
+#output
+    if number >= 51:
+        print('Tails')
+    else:
+        print('Heads')
 
 
-
-
-
-
-
+main()
 
 
 ########### END YER CODE ABOVE THIS LINE ###########
+
+    
+
 
 
 ########################################
@@ -43,25 +64,12 @@ Tails
 '''
 
 1. What was the hardest part of completing this lab? 
+The lab was fairly intuitive thankful for the jupyter notes. I forgot 
+to imput the random number generator and restrict the boundaries 1-100.
 
 
 
 
 
 
-
-'''
-
-########################################
-#            ATTESTATION
-########################################
-'''
-It is critical in this class that you understand the concepts as we explore them because
-those concepts are required understanding for entry level programming. Reliance on resources
-like AI and internet sites like Chegg, CourseHero, StackOverflow, and general Google results
-may impede your understanding. Please rate how well you understand the concepts in this lab: 
-[ ] I understand very little about this lab.
-[ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
-[ ] I pretty much get it.
-[ ] I'm solid. Totally got it.
 '''
